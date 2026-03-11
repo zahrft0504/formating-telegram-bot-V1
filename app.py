082f3d3a9329fd96afde161781ef8365cf526819
zahrft0504 <91391535+zahrft0504@users.noreply.github.com>
2026-03-12 01:06:35 +0200

    Reorganize imports and enhance structure

--- app.py
+++ app.py
@@ -184,7 +184,7 @@ def parse_model_output(text):
 
     return data
 
-LAST_FILE = "last_scheduled.json"
+#LAST_FILE = "last_scheduled.json"
 
 # in-memory state: user_id -> datetime
 pending_schedule = {}
@@ -365,6 +365,7 @@ if __name__ == "__main__":
     # Start Flask web server (required for Render)
     port = int(os.environ.get("PORT", 10000)) #5000 last commit
     app.run(host="0.0.0.0", port=port)
+
 
 
 
